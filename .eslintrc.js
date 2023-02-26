cd01a9642028f1136d6e5cf391bca68e39ef14e5
Tatiana Roslyakova <tat-rs@yandex.ru>
2023-02-26 12:36:23 +0300

    feat: add webpack-bundle-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,7 +52,7 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'warn',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true }],
-    'max-len': ['error', { ignoreComments: true }],
+    'max-len': ['error', { ignoreComments: true, code: 100 }],
   },
   globals: {
     __IS_DEV__: true,
